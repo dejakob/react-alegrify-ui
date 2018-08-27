cf111e330e0a714847113ada69a6139ac3aff5c5
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2018-08-27 20:08:28 +0200

    add constants

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+import COLORS from './lib/colors';
+import MEDIA from './lib/media';
+import SPACING from './lib/spacing';
+
 import Alert from './lib/alert';
 import Checkbox from './lib/checkbox';
 
 export {
+    COLORS,
+    MEDIA,
+    SPACING,
+
     Alert,
     Checkbox
 };
