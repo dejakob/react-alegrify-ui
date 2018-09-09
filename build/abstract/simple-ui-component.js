7f63158f2efc5903ea34b8d32cf66b4c44951849
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2018-09-09 19:51:07 +0200

    Object assign instead of spread

--- build/abstract/simple-ui-component.js
+++ build/abstract/simple-ui-component.js
@@ -1,1 +1,40 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.SimpleUiComponentPropTypes = undefined;
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var SimpleUiComponentPropTypes = {
+    children: _propTypes2.default.oneOfType([_propTypes2.default.arrayOf(_propTypes2.default.node), _propTypes2.default.node]).isRequired,
+    className: _propTypes2.default.string
+};
+
+/**
+ * Simple component that passes props and attaches the className
+ * @param {React.Component} el 
+ * @param {String} className 
+ * @param {Object} props 
+ */
+function SimpleUiComponent(el, className, props) {
+    var classNames = [className];
+
+    if (typeof props.className === 'string') {
+        classNames.push(props.className);
+    }
+
+    return _react2.default.cloneElement(el, Object.assign({}, { className: classNames.join(' ') }, props));
+}
+
+exports.default = SimpleUiComponent;
+exports.SimpleUiComponentPropTypes = SimpleUiComponentPropTypes;
--- build/index.js
+++ build/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.Radio = exports.NumberInput = exports.Main = exports.Label = exports.Input = exports.Dialog = exports.Checkbox = exports.Card = exports.Button = exports.Aside = exports.Article = exports.Alert = exports.SPACING = exports.MEDIA = exports.COLORS = undefined;
+exports.Strong = exports.Em = exports.P = exports.H3 = exports.H2 = exports.H1 = exports.Radio = exports.NumberInput = exports.Main = exports.Label = exports.Input = exports.Dialog = exports.Checkbox = exports.Card = exports.Button = exports.Aside = exports.Article = exports.Alert = exports.SPACING = exports.MEDIA = exports.COLORS = undefined;
 
 var _colors = require('./colors');
 
@@ -65,6 +65,8 @@ var _radio = require('./radio');
 
 var _radio2 = _interopRequireDefault(_radio);
 
+var _typography = require('./typography');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.COLORS = _colors2.default;
@@ -82,3 +84,9 @@ exports.Label = _label2.default;
 exports.Main = _main2.default;
 exports.NumberInput = _numberInput2.default;
 exports.Radio = _radio2.default;
+exports.H1 = _typography.H1;
+exports.H2 = _typography.H2;
+exports.H3 = _typography.H3;
+exports.P = _typography.P;
+exports.Em = _typography.Em;
+exports.Strong = _typography.Strong;
