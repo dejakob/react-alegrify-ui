3378669a9be54bca11a6dc3817c6a19ec9a1786f
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-03-06 23:35:38 +0100

    retry gh pages

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -4,7 +4,7 @@ git reset --hard origin/master
 yarn run docz:build
 cp -R .docz/dist/* .
 npm i -g replace-in-file
-replace-in-file '<script src="/static' '<script src="https://raw.githubusercontent.com/dejakob/react-alegrify-ui/gh-pages/static' index.html
+replace-in-file '<script src="/static' '<script src="https://dejakob.github.io/react-alegrify-ui/static' index.html
 git add .
 git commit -am 'gh-pages update'
 git push origin gh-pages --force
