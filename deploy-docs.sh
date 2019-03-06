c49ea702b0ca39936578e48127b095a560748b12
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-03-06 23:06:43 +0100

    deploy docs script

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -1,6 +1,7 @@
 git branch -f gh-pages
 git checkout gh-pages
-git reset --hard origin/master yarn run docz:build
+git reset --hard origin/master
+yarn run docz:build
 cp -R .docz/dist/* .
 git add .
 git commit -am 'gh-pages update'
