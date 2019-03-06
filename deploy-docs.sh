c3245a09be367016ac420794a489cb9a4c1e2321
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-03-06 23:05:42 +0100

    deploy docs scrupt

---
+++ deploy-docs.sh
@@ -0,0 +1,8 @@
+git branch -f gh-pages
+git checkout gh-pages
+git reset --hard origin/master yarn run docz:build
+cp -R .docz/dist/* .
+git add .
+git commit -am 'gh-pages update'
+git push origin gh-pages --force
+git checkout master
