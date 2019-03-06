d7e36de0e13c392dbb0e0305133c2d596514bb88
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-03-06 23:41:42 +0100

    /static by static

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -3,8 +3,7 @@ git checkout gh-pages
 git reset --hard origin/master
 yarn run docz:build
 cp -R .docz/dist/* .
-npm i -g replace-in-file
-replace-in-file '<script src="/static' '<script src="https://dejakob.github.io/react-alegrify-ui/static' index.html
+node replace-all '<script src="/static' '<script src="static' index.html
 git add .
 git commit -am 'gh-pages update'
 git push origin gh-pages --force
