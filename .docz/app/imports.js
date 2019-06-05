f7d0cc017d243fcd11a4b73e4e25d69ea8415414
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-06-06 00:30:34 +0200

    Downgrade docz

--- .docz/app/imports.js
+++ .docz/app/imports.js
@@ -2,49 +2,95 @@ export const imports = {
   'index.mdx': () =>
     import(/* webpackPrefetch: true, webpackChunkName: "index" */ 'index.mdx'),
   'docs/alert.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-alert" */ 'docs/alert.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-alert" */ 'docs/alert.mdx'
+    ),
   'docs/article.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-article" */ 'docs/article.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-article" */ 'docs/article.mdx'
+    ),
   'docs/aside.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-aside" */ 'docs/aside.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-aside" */ 'docs/aside.mdx'
+    ),
   'docs/button.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-button" */ 'docs/button.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-button" */ 'docs/button.mdx'
+    ),
   'docs/card.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-card" */ 'docs/card.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-card" */ 'docs/card.mdx'
+    ),
   'docs/checkbox.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-checkbox" */ 'docs/checkbox.mdx'),
-  'docs/dialog.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-dialog" */ 'docs/dialog.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-checkbox" */ 'docs/checkbox.mdx'
+    ),
   'docs/date-indicator.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-date-indicator" */ 'docs/date-indicator.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-date-indicator" */ 'docs/date-indicator.mdx'
+    ),
+  'docs/dialog.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-dialog" */ 'docs/dialog.mdx'
+    ),
   'docs/dl.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-dl" */ 'docs/dl.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-dl" */ 'docs/dl.mdx'
+    ),
   'docs/dropdown.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-dropdown" */ 'docs/dropdown.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-dropdown" */ 'docs/dropdown.mdx'
+    ),
   'docs/footer.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-footer" */ 'docs/footer.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-footer" */ 'docs/footer.mdx'
+    ),
   'docs/grid.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-grid" */ 'docs/grid.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-grid" */ 'docs/grid.mdx'
+    ),
   'docs/historic-item.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-historic-item" */ 'docs/historic-item.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-historic-item" */ 'docs/historic-item.mdx'
+    ),
   'docs/input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-input" */ 'docs/input.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-input" */ 'docs/input.mdx'
+    ),
   'docs/label.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-label" */ 'docs/label.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-label" */ 'docs/label.mdx'
+    ),
   'docs/main.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-main" */ 'docs/main.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-main" */ 'docs/main.mdx'
+    ),
   'docs/number-input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-number-input" */ 'docs/number-input.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-number-input" */ 'docs/number-input.mdx'
+    ),
   'docs/radio.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-radio" */ 'docs/radio.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-radio" */ 'docs/radio.mdx'
+    ),
   'docs/range-input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-range-input" */ 'docs/range-input.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-range-input" */ 'docs/range-input.mdx'
+    ),
   'docs/section.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-section" */ 'docs/section.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-section" */ 'docs/section.mdx'
+    ),
   'docs/spacing.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-spacing" */ 'docs/spacing.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-spacing" */ 'docs/spacing.mdx'
+    ),
   'docs/status-input.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-status-input" */ 'docs/status-input.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-status-input" */ 'docs/status-input.mdx'
+    ),
   'docs/typography.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "docs-typography" */ 'docs/typography.mdx'),
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "docs-typography" */ 'docs/typography.mdx'
+    ),
 }
