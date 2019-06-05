d57256ee1073326fa263d1d1c1d8fe82367efa8a
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2019-06-05 23:38:27 +0200

    Upgrade build tools

---
+++ build/helpers/universal-props.js
@@ -0,0 +1,42 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.getAdditionalClassNames = getAdditionalClassNames;
+exports.attachUniversalProps = attachUniversalProps;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _extends() { _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+
+var UNIVERSAL_CLASSES = {
+  spaceXS: 'alegrify-space--extra-small',
+  spaceS: 'alegrify-space--small',
+  spaceM: 'alegrify-space--medium',
+  spaceL: 'alegrify-space--large',
+  spaceXL: 'alegrify-space--extra-large'
+};
+
+function getAdditionalClassNames(props) {
+  var classNames = (props.className || '').split(' ');
+  Object.keys(UNIVERSAL_CLASSES).forEach(function (propName) {
+    var className = UNIVERSAL_CLASSES[propName];
+
+    if (props[propName]) {
+      classNames.push(className);
+    }
+  });
+  return classNames;
+}
+
+function attachUniversalProps(Component) {
+  return function (props) {
+    var classNames = getAdditionalClassNames(props);
+    return _react["default"].createElement(Component, _extends({}, props, {
+      className: classNames.join(' ')
+    }));
+  };
+}
