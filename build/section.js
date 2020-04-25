1d36be8d98a34ccbc1e00f3ea61fd97b8f2209ef
Jakob Kerkhove <jakob.kerkhove@gmail.com>
2020-04-25 23:50:40 +0200

    section plain variant

--- build/section.js
+++ build/section.js
@@ -36,7 +36,7 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToAr
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
 var CSS_CLASSNAME = 'alegrify-section';
-var VARIANTS = ['large'];
+var VARIANTS = ['large', 'plain'];
 /**
  * <Section />
  */
